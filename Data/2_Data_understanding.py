72a79b2768eb6520880aa44e1df039646766f202
dipalikininge1508 <dipali.kininge1508@gmail.com>
2026-09-14 20:17:16 +0530

    Update 2_Data_understanding.py

--- Data/2_Data_understanding.py
+++ Data/2_Data_understanding.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import gdown
+import os
 
-# Load dataset
-df = pd.read_csv("../Data/APL_Logistics.csv", encoding="latin1")
+file_id = "1WIncON4DKmSUIqdttb-MD0vAhvfQutbM"
+csv_path = "data/APL_Logistics.csv"
+
+os.makedirs("data", exist_ok=True)
+
+if not os.path.exists(csv_path):
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, csv_path, quiet=False)
+
+df = pd.read_csv(csv_path, encoding="latin1")
 
 # Shape
 print("Shape:")
