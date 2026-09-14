8b9b96c8887af164c15a1c96096ca3c24abc7b34
dipalikininge1508 <dipali.kininge1508@gmail.com>
2026-09-14 20:41:06 +0530

    Update 4_EDA_Delivery.py

--- Data/4_EDA_Delivery.py
+++ Data/4_EDA_Delivery.py
@@ -1,18 +1,23 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
+import pandas as pd
 import gdown
 import os
-
-file_id = "1WIncON4DKmSUIqdttb-MD0vAhvfQutbM"
-csv_path = "data/APL_Logistics.csv"
-
-os.makedirs("data", exist_ok=True)
-
-if not os.path.exists(csv_path):
-    url = f"https://drive.google.com/uc?id={file_id}"
-    gdown.download(url, csv_path, quiet=False)
-
-df = pd.read_csv(csv_path, encoding="latin1")
+FILE_ID = "1WIncON4DKmSUIqdttb-MD0vAhvfQutbM"
+CSV_PATH = "data/APL_Logistics.csv"
+@st.cache_data
+def load_data():
+    os.makedirs("data", exist_ok=True)
+    if not os.path.exists(CSV_PATH):
+        url = f"https://drive.google.com/uc?id={FILE_ID}"
+        gdown.download(
+            url,
+            CSV_PATH,
+            quiet=False
+        )
+    return pd.read_csv(CSV_PATH)
+    
+df = load_data()
 
 # Calculate delay gap
 df["Delay_Gap"] = (
