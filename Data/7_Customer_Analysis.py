610009173ef9bf969f8a18bad0735df17220f8ef
dipalikininge1508 <dipali.kininge1508@gmail.com>
2026-09-14 20:19:28 +0530

    Update 7_Customer_Analysis.py

--- Data/7_Customer_Analysis.py
+++ Data/7_Customer_Analysis.py
@@ -1,7 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import gdown
+import os
 
-df = pd.read_csv("../Data/APL_Logistics.csv", encoding="latin1")
+file_id = "1WIncON4DKmSUIqdttb-MD0vAhvfQutbM"
+csv_path = "data/APL_Logistics.csv"
+
+os.makedirs("data", exist_ok=True)
+
+if not os.path.exists(csv_path):
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, csv_path, quiet=False)
+
+df = pd.read_csv(csv_path, encoding="latin1")
 
 df["Delay_Gap"] = (
     df["Days for shipping (real)"]
