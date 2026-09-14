37acf9fbdb443bbb13bd6b7e7533f649ec094a2d
dipalikininge1508 <dipali.kininge1508@gmail.com>
2026-09-14 20:25:41 +0530

    Update 9_Dashboard.py

--- Data/9_Dashboard.py
+++ Data/9_Dashboard.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import gdown
+import os
 
 
 # ============================================================
@@ -36,10 +38,16 @@ st.markdown(
 @st.cache_data
 def load_data():
 
-    df = pd.read_csv(
-        "../Data/APL_Logistics.csv",
-        encoding="latin1"
-    )
+    file_id = "1WIncON4DKmSUIqdttb-MD0vAhvfQutbM"
+    csv_path = "data/APL_Logistics.csv"
+
+    os.makedirs("data", exist_ok=True)
+
+    if not os.path.exists(csv_path):
+        url = f"https://drive.google.com/uc?id={file_id}"
+        gdown.download(url, csv_path, quiet=False)
+
+    df = pd.read_csv(csv_path, encoding="latin1")
 
     df["Delay_Gap"] = (
         df["Days for shipping (real)"]
